95c138377a062e7fd0fb4f507f1057db6925244c
Anika Kumar <66550878+kumaranika@users.noreply.github.com>
2020-06-07 14:37:37 +0530

    created product.cpp
    program to give product of two numbers

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float i,j,p;
+    cout << "Enter two numbers: " << endl;
+    cin >> i >> j;
+    p = i * j;
+    cout << "Product: " << p << endl;
+    return 0;
+}
